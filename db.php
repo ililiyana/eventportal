d1c7b338bfaf9dd24225372581dac6e5e9a124d1
ililiyana <iliyanadagan@gmail.com>
2025-07-07 03:06:27 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,28 +1,12 @@
 <?php
-$connectionString = getenv("DB_CONNECTION_STRING");
+$host = "eventhorizonmysql.mysql.database.azure.com";
+$username = "mysqladmin@eventhorizonmysql";
+$password = "YourPassword";
+$dbname = "eventhorizon";
 
-if (!$connectionString) {
-    die("Environment variable DB_CONNECTION_STRING is not set.");
-}
-
-if (!preg_match("/Server=(.*);Initial Catalog=(.*);User ID=(.*);Password=(.*);/", $connectionString, $matches)) {
-    die("Failed to parse DB_CONNECTION_STRING.");
-}
-
-$server = $matches[1];
-$database = $matches[2];
-$user = $matches[3];
-$pass = $matches[4];
-
-$connectionOptions = [
-    "Database" => $database,
-    "UID" => $user,
-    "PWD" => $pass,
-    "Encrypt" => true
-];
-
-$conn = sqlsrv_connect($server, $connectionOptions);
-if (!$conn) {
-    die(print_r(sqlsrv_errors(), true));
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
