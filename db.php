246dbc902f5fd401cbd3d7f1624fec149f89d76f
ililiyana <iliyanadagan@gmail.com>
2025-07-07 12:29:53 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $database = getenv("DB_NAME");
 $username = getenv("DB_USER");
 $password = getenv("DB_PASS");
 
-$conn = new mysqli($server, $username, $password, $database);
+$conn = new mysqli($server, $name, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
